416576dba56dcac86e45b08df25ad8d1a86ab03b
Ovakoma <ideleovercomer18@gmail.com>
2022-06-02 06:02:33 -0700

    a C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *  main: print the strings in the puts function
- *  Description:using the main function this prints 
- *  "Programming is like building a multilingual puzzle
- *  return: 0
- */
+ * main - print the strings in the puts function
+ * Description: prints "Programming is like building a multilingual puzzle
+ * Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
