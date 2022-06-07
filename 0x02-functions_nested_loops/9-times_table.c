7af0ca3f7862cd7e75d5a0da254c6c6be997feb9
Ovakoma <ideleovercomer18@gmail.com>
2022-06-07 13:55:57 -0700

    functions andloop

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting with 0
+ * Return: return times table result
+ */
+void times_table(void)
+{
+	int i;
+	int s = 9;
+
+	for (i = 0; i <= 9; i++)
+	{
+		s *= i;
+	}
+}
