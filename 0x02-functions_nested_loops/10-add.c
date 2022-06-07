b69be126663fb6472947dba48eaaa4f133bc5931
Ovakoma <ideleovercomer18@gmail.com>
2022-06-07 15:44:17 -0700

    function declaration

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - To add two integers and return result
+ * @i: one of the values to b added
+ * @j: one of the values to be added
+ * Return: return integer value of result
+ */
+int add(int i, int j)
+{
+	int i, j, s;
+
+	s = i + j;
+	return (s);
+}
