2d339bf1c9187ae3b8e02c4d107dbf1216f58263
Ovakoma <ideleovercomer18@gmail.com>
2022-06-07 16:01:11 -0700

    function prints range of numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @n: the start of the range of numbers to be printed
+ * Return: return range of numbers
+ */
+void print_to_98(int n)
+{
+	int stop;
+
+	while (n < 99)
+	{
+		n++;
+	}
+}
