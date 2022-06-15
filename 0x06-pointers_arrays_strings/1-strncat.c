cba3c49e90d641b5b1a3a88d87065ad7a6629160
Ovakoma <ideleovercomer18@gmail.com>
2022-06-15 13:21:47 -0700

    Pointers and strings and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (*src != '\0' && j < n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		j++;
