67377449f86f957a4edcad228f3bcc753497cf19
Ovakoma <ideleovercomer18@gmail.com>
2022-07-14 10:20:01 -0700

    prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; t != NULL; i++)
 	{
 		if (t->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		printf("[%d] %s\n", t->len, t->str);
 		t = t->next;
 	}
