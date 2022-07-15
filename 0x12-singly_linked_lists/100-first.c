0d5253bfa45ef525b00772977b1477f2a66b7809
Ovakoma <ideleovercomer18@gmail.com>
2022-07-15 04:03:17 -0700

    prints from init by the loader befor main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,9 @@
 /**
  * _p - prints from init by the loader befor main
  */
-
 void _p(void)__attribute__ ((constructor));
 void _p(void)
+
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
