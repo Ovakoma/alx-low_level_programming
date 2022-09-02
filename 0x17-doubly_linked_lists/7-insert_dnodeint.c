8ef04487202e9bb7f2e1d0036c9dae39f443e00c
Ovakoma <ideleovercomer18@gmail.com>
2022-09-01 17:01:08 -0700

    Updated 0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
-		idx++;
+		idx--;
 	}
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
