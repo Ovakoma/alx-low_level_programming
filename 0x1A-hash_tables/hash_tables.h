bdb58658df07e3b151113070bc271fc87e24fe42
Ovakoma <ideleovercomer18@gmail.com>
2023-01-06 22:36:04 +0100

    Updated 0x1A-hash_tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -87,5 +87,12 @@ void shash_table_print_rev(const shash_table_t *ht);
 char *str_copy1(const char *s);
 void insert_shash_node(shash_table_t *ht, shash_node_t *hn);
 unsigned long int hash_djb2(const unsigned char *str);
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+char *str_copy(const char *s);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
